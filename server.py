90917d06d08736db3e9747fad5ac45d7dc12a8d1
Miguel <Migue@LAPTOP-91M8P1HF>
2026-09-14 11:55:41 -0600

    Saludo desde python

--- server.py
+++ server.py
@@ -8,9 +8,20 @@ port = 3000
 
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
+        urlParse = urlparse(self.path)
+        qs = parse_qs(urlParse.query)
+       
+        if urlParse.path == "/saludo":
+            saludo = qs["nombre"][0] + " bienvenido a Python"
+            
+            self.send_response(200)
+            self.send_header("Content-type","text/html")
+            self.end_headers()
+            self.wfile.write(saludo.encode("utf-8"))
+
         if self.path == "/":
             self.path = "/index.html"
-        return SimpleHTTPRequestHandler.do_GET(self)
+            return SimpleHTTPRequestHandler.do_GET(self)
 
 print(f"Servidor corriendo en el puerto {port}")
 server = HTTPServer(("localhost",port),miServidor)
